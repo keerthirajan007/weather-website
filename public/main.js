cc9df44ca74443134e4b76ee31a231f0c8b81d7d
keerthirajan007 <capkeerthi@gmail.com>
2020-08-12 17:46:05 +0530

    bugs

--- public/main.js
+++ public/main.js
@@ -6,12 +6,78 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const message = document.getElementsByClassName("message");
 const space = document.getElementsByClassName("space");
-const gps = document.getElementsByClassName("gps");
+const gps = document.querySelector(".gps");
 
 message[0].innerHTML =
   "To see Weather information,please provide a place name in the search box";
 input.addEventListener("onclick", () => {
   input.style.background = "white";
+});
+gps.addEventListener("click", () => {
+  console.log("success");
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }
+  function showPosition(position) {
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+    value[0].innerHTML = "";
+    address = input.value;
+    message[0].innerHTML = "Loading... please wait";
+    fetch(
+      `/weather/gps?lat=${position.coords.latitude}&lon=${position.coords.longitude}`
+    ).then((response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          console.log(data.error);
+          message[0].innerHTML = data.error;
+        } else {
+          message[0].innerHTML = "Done...";
+          space[0].innerHTML = "";
+          value[0].innerHTML = `<table>
+          <tr>
+              <td><b>Place</b></td>
+              <td>: ${data[0].Place}</td>
+          </tr>
+          <tr>
+              <td><b>Latitude</b></td>
+              <td>: ${position.coords.latitude}</td>
+          </tr>
+          <tr>
+              <td><b>Longitude</b></td>
+              <td>: ${position.coords.longitude}</td>
+          </tr>
+          <tr>
+          <td><b>Timezone</b></td>
+          <td>: ${data[0].Timezone}</td>
+          </tr>
+          <tr>
+  <td><b>Place Time</b></td>
+  <td>: ${data[0].PlaceTime}</td>
+  </tr>
+          <tr>
+              <td><b>Weather</b></td>
+              <td>: ${data[0].Weather}</td>
+          </tr>
+          <tr>
+              <td><b>Humidity(g/cm<sup>3</sup>)</b></td>
+              <td>: ${data[0].Humidity}</td>
+          </tr>
+          <tr>
+              <td><b>Pressure(Pa)</b></td>
+              <td>: ${data[0].Pressure}</td>
+          </tr>
+          <tr>
+              <td><b>Temperature(K)</b></td>
+              <td>: ${data[0].Temperature}</td>
+          </tr> <tr>
+          <td><b>Date</b></td>
+          <td>: ${data[0].Date}</td>
+      </tr>
+      </table>`;
+        }
+      });
+    });
+  }
 });
 
 form.addEventListener("submit", (e) => {
@@ -72,69 +138,69 @@ form.addEventListener("submit", (e) => {
     });
   });
 });
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  }
-}
+// function getLocation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(showPosition);
+//   }
+// }
 
-function showPosition(position) {
-  var latlon = position.coords.latitude + "," + position.coords.longitude;
-  value[0].innerHTML = "";
-  address = input.value;
-  message[0].innerHTML = "Loading... please wait";
-  fetch(
-    `/weather/gps?lat=${position.coords.latitude}&lon=${position.coords.longitude}`
-  ).then((response) => {
-    response.json().then((data) => {
-      if (data.error) {
-        console.log(data.error);
-        message[0].innerHTML = data.error;
-      } else {
-        message[0].innerHTML = "Done...";
-        space[0].innerHTML = "";
-        value[0].innerHTML = `<table>
-        <tr>
-            <td><b>Place</b></td>
-            <td>: ${data[0].Place}</td>
-        </tr>
-        <tr>
-            <td><b>Latitude</b></td>
-            <td>: ${position.coords.latitude}</td>
-        </tr>
-        <tr>
-            <td><b>Longitude</b></td>
-            <td>: ${position.coords.longitude}</td>
-        </tr>
-        <tr>
-        <td><b>Timezone</b></td>
-        <td>: ${data[0].Timezone}</td>
-        </tr>
-        <tr>
-<td><b>Place Time</b></td>
-<td>: ${data[0].PlaceTime}</td>
-</tr>
-        <tr>
-            <td><b>Weather</b></td>
-            <td>: ${data[0].Weather}</td>
-        </tr>
-        <tr>
-            <td><b>Humidity(g/cm<sup>3</sup>)</b></td>
-            <td>: ${data[0].Humidity}</td>
-        </tr>
-        <tr>
-            <td><b>Pressure(Pa)</b></td>
-            <td>: ${data[0].Pressure}</td>
-        </tr>
-        <tr>
-            <td><b>Temperature(K)</b></td>
-            <td>: ${data[0].Temperature}</td>
-        </tr> <tr>
-        <td><b>Date</b></td>
-        <td>: ${data[0].Date}</td>
-    </tr>
-    </table>`;
-      }
-    });
-  });
-}
+// function showPosition(position) {
+//   var latlon = position.coords.latitude + "," + position.coords.longitude;
+//   value[0].innerHTML = "";
+//   address = input.value;
+//   message[0].innerHTML = "Loading... please wait";
+//   fetch(
+//     `/weather/gps?lat=${position.coords.latitude}&lon=${position.coords.longitude}`
+//   ).then((response) => {
+//     response.json().then((data) => {
+//       if (data.error) {
+//         console.log(data.error);
+//         message[0].innerHTML = data.error;
+//       } else {
+//         message[0].innerHTML = "Done...";
+//         space[0].innerHTML = "";
+//         value[0].innerHTML = `<table>
+//         <tr>
+//             <td><b>Place</b></td>
+//             <td>: ${data[0].Place}</td>
+//         </tr>
+//         <tr>
+//             <td><b>Latitude</b></td>
+//             <td>: ${position.coords.latitude}</td>
+//         </tr>
+//         <tr>
+//             <td><b>Longitude</b></td>
+//             <td>: ${position.coords.longitude}</td>
+//         </tr>
+//         <tr>
+//         <td><b>Timezone</b></td>
+//         <td>: ${data[0].Timezone}</td>
+//         </tr>
+//         <tr>
+// <td><b>Place Time</b></td>
+// <td>: ${data[0].PlaceTime}</td>
+// </tr>
+//         <tr>
+//             <td><b>Weather</b></td>
+//             <td>: ${data[0].Weather}</td>
+//         </tr>
+//         <tr>
+//             <td><b>Humidity(g/cm<sup>3</sup>)</b></td>
+//             <td>: ${data[0].Humidity}</td>
+//         </tr>
+//         <tr>
+//             <td><b>Pressure(Pa)</b></td>
+//             <td>: ${data[0].Pressure}</td>
+//         </tr>
+//         <tr>
+//             <td><b>Temperature(K)</b></td>
+//             <td>: ${data[0].Temperature}</td>
+//         </tr> <tr>
+//         <td><b>Date</b></td>
+//         <td>: ${data[0].Date}</td>
+//     </tr>
+//     </table>`;
+//       }
+//     });
+//   });
+// }
